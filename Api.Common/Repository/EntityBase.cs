03a8c5450cce87d79c1dc93f4ccc01e527567f04
K0d343V3r <zoolook@cox.net>
2018-11-22 20:34:51 -0800

    Removed UpdateFrom() method from EntityBase.

--- Api.Common/Repository/EntityBase.cs
+++ Api.Common/Repository/EntityBase.cs
@@ -8,16 +8,5 @@ namespace Api.Common.Repository
     public abstract class EntityBase
     {
         public int Id { get; set; }
-
-        protected void UpdateFrom(EntityBase fromEntity)
-        {
-            Id = fromEntity.Id;
-            var toSortable = this as ISortable;
-            var fromSortable = fromEntity as ISortable;
-            if (toSortable != null && fromSortable != null)
-            {
-                toSortable.Position = fromSortable.Position;
-            }
-        }
     }
 }
