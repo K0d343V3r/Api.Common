4e71b89f8df81428e92a4ea774c6d613a6f8eb70
K0d343V3r <zoolook@cox.net>
2019-01-09 20:38:58 -0800

    Added Redis cache support.

---
+++ Api.Common/Cache/IEntityCache.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Common.Cache
+{
+    public interface IEntityCache<T>
+    {
+        Task<T> GetEntityAsync(string prefix, string key);
+        Task SetEntityAsync(string prefix, string key, T entity);
+        Task SetEntityAsync(string prefix, string key, T entity, TimeSpan? expirationFromNow);
+        Task InvalidateEntityAsync(string prefix, string key);
+    }
+}
---
+++ Api.Common/Cache/RedisEntityCache.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Common.Cache
+{
+    public class RedisEntityCache<T> : IEntityCache<T>
+    {
+        private readonly IDistributedCache _cache;
+
+        public RedisEntityCache(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public async Task<T> GetEntityAsync(string prefix, string key)
+        {
+            string entityString = await _cache.GetStringAsync(GetCacheKey(prefix, key));
+            if (string.IsNullOrEmpty(entityString))
+            {
+                return default(T);
+            }
+            else
+            {
+                return JsonConvert.DeserializeObject<T>(entityString);
+            }
+        }
+
+        public async Task SetEntityAsync(string prefix, string key, T entity)
+        {
+            await SetEntityAsyncInternal(prefix, key, entity, null);
+        }
+
+        public async Task SetEntityAsync(string prefix, string key, T entity, TimeSpan? expirationFromNow)
+        {
+            await SetEntityAsyncInternal(prefix, key, entity, expirationFromNow);
+        }
+
+        private async Task SetEntityAsyncInternal(string prefix, string key, T entity, TimeSpan? expirationFromNow)
+        {
+            await _cache.SetStringAsync(
+                 GetCacheKey(prefix, key),
+                 JsonConvert.SerializeObject(entity),
+                 new DistributedCacheEntryOptions()
+                 {
+                     AbsoluteExpirationRelativeToNow = expirationFromNow
+                 }
+             );
+        }
+
+        public async Task InvalidateEntityAsync(string prefix, string key)
+        {
+            await _cache.RemoveAsync(GetCacheKey(prefix, key));
+        }
+
+        private string GetCacheKey(string prefix, string key)
+        {
+            return $"{prefix}-{key}";
+        }
+    }
+}
