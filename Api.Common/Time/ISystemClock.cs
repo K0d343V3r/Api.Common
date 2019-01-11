c459f8de59b637e8868c0f663f2df3047944e954
K0d343V3r <zoolook@cox.net>
2019-01-11 10:56:31 -0800

    Added system clock service.

---
+++ Api.Common/Time/ISystemClock.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api.Common.Time
+{
+    public interface ISystemClock
+    {
+        DateTime Now { get; }
+        DateTime UtcNow { get; }
+    }
+}
---
+++ Api.Common/Time/SystemClock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api.Common.Time
+{
+    public class SystemClock : ISystemClock
+    {
+        public DateTime Now
+        {
+            get
+            {
+                return DateTime.Now;
+            }
+        }
+
+        public DateTime UtcNow
+        {
+            get
+            {
+                return DateTime.UtcNow;
+            }
+        }
+    }
+}
